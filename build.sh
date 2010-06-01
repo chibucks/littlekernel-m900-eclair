cf9a3763e3f3fb604a3cc39da9ac82c705efd916
Littlejth <d124512@aim.com>
2010-06-01 18:52:47 -0500

    many misc. changes

--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-cd src
-make -j2 CFLAGS=-O3
+cd busybox
+# copmile busybox
+make clean
+cp .config_standard .config
+make -j2 
+rm ../initramfs/sbin/busybox
+cp busybox ../initramfs/sbin/busybox
+
+cd ../src
+# compile kernel
+cp .config_netfilters .config
+make clean
+make -j2 CFLAGS=-O3 # optimize for speed
+
+cd ../working_dir
+# create update.tar
+cp ../src/arch/arm/boot/zImage zImage
+./retar-moment -z LittleKernel.tar
+cd ..
+
+
