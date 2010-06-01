b67d79e5392098dcea13d365b260ff1f8e9cd724
Littlejth <d124512@aim.com>
2010-05-31 19:56:38 -0500

    stupid mistake in build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-	echo *************************************
-	echo *           build kernel            *
-	echo *************************************
-		
-		cd src/kernel
-		make -j2 CFLAGS=-O3
-		if [ $? != 0 ] ; then
-			exit $?
-		fi
-
-echo ""
-echo "Total spent time:"
-times
-	
-exit 0
+cd src
+make -j2 CFLAGS=-O3
